b52052c47a2986b0229c6146498a412ab53a0473
MihaiGoncear <MihaiGoncear@users.noreply.github.com>
2020-06-01 17:25:40 +0300

    --

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 fetch('https://api.nasa.gov/planetary/apod?api_key=onuijb7b2ILtD2BxLz3FOz8lYhd1NfHMFMVz4v3C&date=2020-01-22&hd=true')
     .then(response => response.json())
     .then(data => {
-            let body = document.body;
+            createContent(data);
+})
+
+
+function createContent(info) {
+    let body = document.body;
             let title = document.createElement('h1');
-            title.innerText = data.title;
+            title.innerText = info.title;
 
             let imageDiv = document.createElement('div');
             imageDiv.classList.add('image');
 
             let image = document.createElement('img');
-            image.setAttribute('src', data.hdurl);
+            image.setAttribute('src', info.url);
             imageDiv.append(image);
 
             let paragraph = document.createElement('p');
-            paragraph.innerText = data.explanation;
+            paragraph.innerText = info.explanation;
 
             let copyrightDiv = document.createElement('div');
             copyrightDiv.classList.add('copyright');
             
             let copyrightSpan = document.createElement('span');
-            copyrightSpan.innerHTML = '© ' + data.copyright;
+            copyrightSpan.innerHTML = '© ' + info.copyright;
             copyrightDiv.append(copyrightSpan);
 
             body.append(title);
             body.append(imageDiv);
             body.append(paragraph);
             body.append(copyrightDiv);
-})
-
-
+}
